5b7791c94d2461c1275bacbb6bcd2f841ab27a7e
John Dudley <Jojadud@users.noreply.github.com>
2023-07-21 01:32:17 +0100

    add gripper models

---
+++ Assets/Scripts/GripperController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripperController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Ray ray = new Ray()
+        //OVRInput.Get()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
